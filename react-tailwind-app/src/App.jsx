5ef4ee54f470f1b138d3a3a2f0f42a9548bfbd8e
Admin <Admin@DESKTOP-U15EDUT>
2025-04-24 17:29:00 +0530

    added new

--- react-tailwind-app/src/App.jsx
+++ react-tailwind-app/src/App.jsx
@@ -8,6 +8,9 @@ import ProtectedRoute from "./components/ProtectedRoute";
 import Logout from "./components/Logout";
 import AddCenter from './components/AddCenter';
 import ViewCenters from './components/ViewCenters';
+import AddSchool from './components/AddSchool';
+import ViewSchools from './components/ViewSchools';
+import AddTeacher from './components/AddTeacher';
 
 function App() {
   return (
@@ -18,6 +21,9 @@ function App() {
         <Route path="/logout" element={<Logout />} />
         <Route path="/dashboard/masters/center/add" element={<AddCenter />} />
         <Route path="/dashboard/masters/center/view" element={<ViewCenters />} />
+        <Route path="/dashboard/masters/school/add" element={<AddSchool />} />
+        <Route path="/dashboard/masters/school/view" element={<ViewSchools />} />
+        <Route path="/dashboard/masters/teacher/add" element={<AddTeacher />} />
         <Route path="/register" element={<StudentForm />} />
 
         <Route
